702ef3052a39d925168e748893202857e412e94c
akiffkocc <akiffkocc60@gmail.com>
2024-08-23 18:16:44 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductDetailServices/IProductDetailService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catalog.Dtos.ProductDetailDtos;
+
+namespace MultiShop.Catalog.Services.ProductDetailServices
+{
+    public interface IProductDetailService
+    {
+        Task<List<ResultProductDetailDto>> GetAllProductDetailAsync();
+        Task CreateProductDetailAsync(CreateProductDetailDto createProductDetailDto0);
+        Task UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailDto0);
+        Task DeleteProductDetailAsync(string id);
+        Task<GetByIdProductDetailDto> GetByIdProductDetailAsync(string id);
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Catalog.Dtos.ProductDetailDtos;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+
+namespace MultiShop.Catalog.Services.ProductImageServices
+{
+    public interface IProductImageService
+    {
+        Task<List<ResultProductImageDto>> GetAllProductImageAsync();
+        Task CreateProductImageAsync(CreateProductImageDto createProductImageDto0);
+        Task UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto0);
+        Task DeleteProductImageAsync(string id);
+        Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id);
+    }
+}
